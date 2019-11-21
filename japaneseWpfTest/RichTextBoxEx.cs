87038ff706ca19171c871beac12a7d4b31c86aef
Lee Hanhyeong <forestcamp11@gmail.com>
2019-11-21 10:44:47 +0900

    이미지 추가

--- japaneseWpfTest/RichTextBoxEx.cs
+++ japaneseWpfTest/RichTextBoxEx.cs
@@ -13,49 +13,49 @@ namespace japaneseWpfTest
         {
             this.PreviewKeyDown += (s, e) =>
             {
-                if ((Keyboard.IsKeyDown(Key.LeftShift)) && Keyboard.IsKeyDown(Key.Enter))
-                {
-                    var richTb = (s as RichTextBoxEx);
+                //if ((Keyboard.IsKeyDown(Key.LeftShift)) && Keyboard.IsKeyDown(Key.Enter))
+                //{
+                //    var richTb = (s as RichTextBoxEx);
 
-                    // 일본어 또는 중국어 입력시 생성되는 단어교환기? 팝업 (CompositionAdorner class)이 있을시
-                    // shift + enter 기능을 방지한다.
-                    if (richTb.CheckCompositionAdorner()) return;
+                //    // 일본어 또는 중국어 입력시 생성되는 단어교환기? 팝업 (CompositionAdorner class)이 있을시
+                //    // shift + enter 기능을 방지한다.
+                //    if (richTb.CheckCompositionAdorner()) return;
 
-                    // 현재 캐럿위치의 paragraph를 구함
-                    var currentPara = richTb.CaretPosition.Paragraph;
+                //    // 현재 캐럿위치의 paragraph를 구함
+                //    var currentPara = richTb.CaretPosition.Paragraph;
 
-                    // 선택되어진 text 제거
-                    var startPointer = richTb.Selection.Start;
-                    var endPointer = richTb.Selection.End;
-                    richTb.Selection.Text = "";
+                //    // 선택되어진 text 제거
+                //    var startPointer = richTb.Selection.Start;
+                //    var endPointer = richTb.Selection.End;
+                //    richTb.Selection.Text = "";
 
-                    // shift + enter시 다음줄로 내려야할 텍스트를 복사해 놓는다.
-                    var tr = new TextRange(startPointer, currentPara.ContentEnd);
-                    var selText = tr.Text;
+                //    // shift + enter시 다음줄로 내려야할 텍스트를 복사해 놓는다.
+                //    var tr = new TextRange(startPointer, currentPara.ContentEnd);
+                //    var selText = tr.Text;
 
-                    tr.Text = "";
-                    //Console.WriteLine(selText);
+                //    tr.Text = "";
+                //    //Console.WriteLine(selText);
 
-                    // 신규 paragraph 추가
-                    var para = new Paragraph();
-                    // 신규 마진 적용
-                    para.Margin = new Thickness();
+                //    // 신규 paragraph 추가
+                //    var para = new Paragraph();
+                //    // 신규 마진 적용
+                //    para.Margin = new Thickness();
 
-                    // shift + enter 시 아랫줄로 내려가야 할 텍스트가 있다면
-                    // 신규 paragraph에 추가
-                    if (!string.IsNullOrEmpty(selText))
-                    {
-                        para.Inlines.Add(selText);
-                    }
+                //    // shift + enter 시 아랫줄로 내려가야 할 텍스트가 있다면
+                //    // 신규 paragraph에 추가
+                //    if (!string.IsNullOrEmpty(selText))
+                //    {
+                //        para.Inlines.Add(selText);
+                //    }
 
-                    // 현재 paragraph 뒤에 생성한 paragraph를 추가한다.
-                    richTb.Document.Blocks.InsertAfter(currentPara, para);
+                //    // 현재 paragraph 뒤에 생성한 paragraph를 추가한다.
+                //    richTb.Document.Blocks.InsertAfter(currentPara, para);
 
-                    // 캐럿을 다음 라인으로 이동해 준다.
-                    richTb.CaretPosition = richTb.CaretPosition.GetLineStartPosition(1);
+                //    // 캐럿을 다음 라인으로 이동해 준다.
+                //    richTb.CaretPosition = richTb.CaretPosition.GetLineStartPosition(1);
 
-                    e.Handled = true;
-                }
+                //    e.Handled = true;
+                //}
             };
         }
 
